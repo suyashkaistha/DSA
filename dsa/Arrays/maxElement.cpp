4828a0a950f9a92f7e962b4fe928033c6bc20301
Suyash Kaistha <suyashkaistha07@gmail.com>
2026-08-06 17:59:08 +0530

    Update maxElement.cpp

--- dsa/Arrays/maxElement.cpp
+++ dsa/Arrays/maxElement.cpp
@@ -5,7 +5,7 @@ int main(){
     int arr[] = {-62,-35,-5,-1,-88,-34};
     int n = sizeof(arr)/4;
     int mx = INT_MIN;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++) {
         // if(arr[i] > mx) mx = arr[i];
         mx = max(mx,arr[i]);
     }
@@ -13,5 +13,5 @@ int main(){
     // for(int i=1;i<n;i++){
     //     if(arr[i] > max) max = arr[i];
     // }
-    cout<<mx;
+    cout<<mx ;
 }
